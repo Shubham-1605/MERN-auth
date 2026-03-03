06571317042b5566c9fe8132d986e9714d0d5fbb
Shubham-1605 <shubham1605dj@gmail.com>
2026-03-03 19:06:00 +0530

    casing changed in login.js

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { AppContext } from "../context/Appcontext";
+import { AppContext } from "../context/AppContext";
 import { toast } from "react-toastify";
 
 const Login = () => {
